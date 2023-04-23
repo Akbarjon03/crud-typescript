6a58f29f57ce2bc624149ce4ec3d5be478ff770d
Akbarjon03 <akbarjonodilov03@gmail.com>
2023-04-23 20:47:53 +0500

    #

--- src/components/EmployeeList.tsx
+++ src/components/EmployeeList.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IEmplopee } from './Employee.type';
 import EmployeeModal from './EmployeeModal';
-import { useState } from 'react';
 
 type Props = {
-  list: IEmplopee[]
+  list: IEmplopee[];
   onDeleteClickHnd: (data: IEmplopee) => void;
-}
+};
+
 const EmployeeList = (props: Props) => {
   const { list, onDeleteClickHnd } = props;
   const [showModel, setShowModel] = useState(false);
+  const [dataToShow, setDataToShow] = useState();
+
   const viewEmployee = () => {
     setShowModel(true);
-  }
+  };
+
+  const onCloseModal = () => setShowModel(false);
+
   return (
     <div className="container">
       <table className="table">
@@ -24,28 +29,26 @@ const EmployeeList = (props: Props) => {
             <th scope="col">Actions</th>
           </tr>
         </thead>
-        {list.map((employee) => {
-          return (
-            <tbody>
-              <tr key={employee.id}>
+        <tbody>
+          {list.map((employee) => (
+            <tr key={employee.id}>
               <th scope="row"></th>
-                <td>{`${employee.firstName} ${employee.lastName}`}</td>
-                <td>{employee.email}</td>
-                <td>
-                  <div className="btn-group" role="group">
-                    <input type="button" className="btn btn-success" value="View" onClick={viewEmployee}/>
-                    <input type="button" className="btn btn-warning" value="Edit"/>
-                    <input type="button" className="btn btn-danger" value="Delete" onClick={() => onDeleteClickHnd(employee)}/>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
+              <td>{`${employee.firstName} ${employee.lastName}`}</td>
+              <td>{employee.email}</td>
+              <td>
+                <div className="btn-group" role="group">
+                  <input type="button" className="btn btn-success" value="View" onClick={()=>viewEmployee(employee)}/>
+                  <input type="button" className="btn btn-warning" value="Edit" />
+                  <input type="button" className="btn btn-danger" value="Delete" onClick={() => onDeleteClickHnd(employee)} />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
-      {showModel && <EmployeeModal/>}
+      {showModel && <EmployeeModal onClick={onCloseModal} data={}/>}
     </div>
-  )
-}
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
--- src/components/EmployeeModal.tsx
+++ src/components/EmployeeModal.tsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import "./EmployeeModal.sytle.css";
+import { IEmplopee } from './Employee.type';
 
-function EmployeeModal() {
+type Props = {
+  onClick: () => void;
+  data: IEmplopee
+};
+
+const EmployeeModal = (props: Props) => {
+  const { onClick } = props;
+
   return (
-      <div id='myModal' className='modal'>
-        <div className='modal-content'>
-          <span className='close'>&times;</span>
-          <p>some text</p>
+    <div id='myModal' className='modal'>
+      <div className='modal-content'>
+        <span className='close' onClick={onClick}>&times;</span>
+        <div>
+          <h3> Employee data</h3>
+          <div>
+            <label> First name : {data.firstName}</label>
+          </div>
+          <div>
+            <label> Last name : {data.lastName}</label>
+          </div>
+          <div>
+            <label> Email Address : {data.email}</label>
+          </div>
         </div>
       </div>
+    </div>
   )
 }
 
-export default EmployeeModal
+export default EmployeeModal;
